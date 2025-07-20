4dff78a9e3025c5668ceb6aa5171154783a443c7
Babymonster <vanhao.dev@gmail.com>
2025-07-20 22:54:34 +0700

    add

---
+++ HaoSocket.Server/Network/Component/SessionComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaoSocket.Server.Network.Component
+{
+    public struct SessionComponent
+    {
+        public Socket Socket;         // Raw socket
+        public EndPoint RemoteEndPoint;
+        public byte[] Buffer;         // Dùng chung buffer
+        public int Offset;            // Offset decode
+        public bool MarkedForDisconnect; // Đánh dấu để Disconnect ở System khác
+    }
+}
