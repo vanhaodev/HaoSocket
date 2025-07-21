df83e83f4595ff4d48637e9aa4144f8d78890be5
BabyMonster <vanhao.dev@gmail.com>
2025-07-21 17:59:24 +0700

    add

---
+++ HaoSocket.Server/Network/Component/TcpReadBufferComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaoSocket.Server.Network.Component
+{
+    public struct TcpReadBufferComponent
+    {
+        public byte[] Buffer;   // Buffer nhận dữ liệu
+        public int Start;       // Dữ liệu chưa xử lý bắt đầu từ đây
+        public int End;         // Dữ liệu đã nhận đến đây
+    }
+}
---
+++ HaoSocket.Server/Network/Component/TcpSocketComponent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaoSocket.Server.Network.Component
+{
+    public struct TcpSessionComponent
+    {
+        public Socket Socket;                // Socket TCP
+        public EndPoint RemoteEndPoint;      // IP:Port
+        public int SessionId;                // ID session nội bộ
+        public bool IsDisconnected;          // Đánh dấu đã ngắt kết nối
+
+        public void Init(Socket socket, int sessionId)
+        {
+            Socket = socket;
+            RemoteEndPoint = socket.RemoteEndPoint!;
+            SessionId = sessionId;
+            IsDisconnected = false;
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                try
+                {
+                    return !(IsDisconnected || (Socket.Poll(1, SelectMode.SelectRead) && Socket.Available == 0));
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (IsDisconnected) return;
+            IsDisconnected = true;
+            try { Socket.Shutdown(SocketShutdown.Both); } catch { }
+            try { Socket.Close(); } catch { }
+        }
+    }
+}
